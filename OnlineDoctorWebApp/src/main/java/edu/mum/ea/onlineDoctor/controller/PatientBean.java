e487e39ee9e49b72d2a6f9f7a45e7ff672f97b66
Fetiya <Fetiya@10.10.14.121>
2014-12-01 11:07:11 -0600

    new view and bean for patient info view

---
+++ OnlineDoctorWebApp/src/main/java/edu/mum/ea/onlineDoctor/controller/PatientBean.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package edu.mum.ea.onlineDoctor.controller;
+
+import javax.ejb.EJB;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+/**
+ *
+ * @author Fetiya
+ */
+@ManagedBean
+@SessionScoped
+public class PatientBean {
+
+   
+   
+    
+    public PatientBean() {
+    }
+    
+}
