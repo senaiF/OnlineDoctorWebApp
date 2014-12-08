52bf41b0a69dfe426f46a960c1dcd045e1f130ba
HailelulLakew <HailelulLakew@10.10.52.134>
2014-12-07 23:37:56 -0600

    AppointmentServiceBean created and conflicts resolved

---
+++ OnlineDoctorWebApp/src/main/java/edu/mum/ea/onlineDoctor/controller/AdministratorViewBean.java
@@ -0,0 +1,101 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package edu.mum.ea.onlineDoctor.controller;
+
+import edu.mum.ea.onlineDoctor.entity.Address;
+import edu.mum.ea.onlineDoctor.entity.Doctor;
+import edu.mum.ea.onlineDoctor.entity.Patient;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+import javax.faces.bean.SessionScoped;
+import javax.inject.Named;
+
+/**
+ *
+ * @author HailelulLakew
+ */
+
+@ManagedBean(name="AdminBean")  
+@SessionScoped
+public class AdministratorViewBean {
+
+    /**
+     * Creates a new instance of ViewPatientAdministrator
+     */
+    private Patient patient;
+    private Address address;
+    @PostConstruct    
+    public void init() { 
+        setAddress(new Address());
+        setPatient (new Patient());   
+        
+        DateFormat df = new SimpleDateFormat("MM/dd/yyyy");
+        Date today = Calendar.getInstance().getTime(); 
+        
+        getAddress().setCity("Fairfield");       
+        getAddress().setState("Iowa");
+     //   getAddress().setStreet("North 1000");        
+        patient.setAddress(getAddress());          
+        patient.setDateofBirth(today);
+        patient.setFirstName("David");
+        patient.setLastName("Johanson");
+        patient.setCellPhoneNo("45238903456");        
+        patient.setHomePhoneNo("623689999999");
+        patient.setWorkPhoneNo("15634688888");
+        patient.setUserName("dfdfd");
+        patient.setUserPassword("fdff");
+        //System.out.println("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+
+    /**
+     * @return the patient
+     */
+    public Patient getPatient() {
+        return patient;
+    }
+
+    /**
+     * @param patient the patient to set
+     */
+    public void setPatient(Patient patient) {
+        this.patient = patient;
+    }
+
+    /**
+     * @return the address
+     */
+    public Address getAddress() {
+        return address;
+    }
+
+    /**
+     * @param address the address to set
+     */
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    
+}
---
+++ OnlineDoctorWebApp/src/main/java/edu/mum/ea/onlineDoctor/controller/DoctorViewBean.java
@@ -0,0 +1,158 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.mum.ea.onlineDoctor.controller;
+
+import edu.mum.ea.onlineDoctor.entity.Address;
+import edu.mum.ea.onlineDoctor.entity.Appointment;
+import edu.mum.ea.onlineDoctor.entity.Doctor;
+import edu.mum.ea.onlineDoctor.entity.MedicalRecord;
+import edu.mum.ea.onlineDoctor.entity.Patient;
+import edu.mum.ea.onlineDoctor.facade.AppointmentFacade;
+import edu.mum.ea.onlineDoctor.facade.DoctorFacade;
+import edu.mum.ea.onlineDoctor.facade.PatientFacade;
+import java.io.Serializable;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+import javax.annotation.PostConstruct;
+import javax.ejb.EJB;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+import javax.faces.bean.SessionScoped;
+import javax.inject.Named;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author HailelulLakew
+ */
+
+@ManagedBean(name="DoctorView") 
+@SessionScoped
+public class DoctorViewBean implements Serializable{
+
+    /**
+     * Creates a new instance of viewPatientDoctor
+     */
+    private List<Doctor> doctors;
+    private ArrayList<Patient> patients;
+    private List<MedicalRecord> medicalRecords;
+    @EJB
+    private PatientFacade patientFacade;
+    
+    @EJB
+    private AppointmentFacade appointmentFacade;
+    
+
+    public DoctorViewBean() {
+    }
+    
+    
+    @PostConstruct    
+    public void init() { 
+        Address address = new Address();
+        Doctor doctor = new Doctor();
+        
+        
+        MedicalRecord medicalRecord= new MedicalRecord();        
+        setMedicalRecords(new ArrayList<MedicalRecord>()); 
+        doctors = new ArrayList<Doctor>();
+        
+        DateFormat df = new SimpleDateFormat("MM/dd/yyyy");
+        Date today = Calendar.getInstance().getTime();
+        medicalRecord.setRemarks("Need Attantion");
+        medicalRecord.setRecordDate(today);
+        medicalRecord.setSymptoms("Head ache and eching");        
+      //  doctor.getMedicalRecords().add(medicalRecord);
+        address.setCity("Fairfield");
+        
+        address.setState("Iowa");
+        //address.setStreetAddress("North 1000");        
+        doctor.setAddress(address);          
+        doctor.setDateofBirth(today);
+        doctor.setFirstName("David");
+        doctor.setLastName("Johanson");
+        doctor.setCellPhoneNo("45238903456");
+       // doctor.setCellPhoneNo("45238903");
+        doctor.setHomePhoneNo("623689999");
+        doctor.setWorkPhoneNo("156346888");
+        doctors.add(doctor);
+        System.out.println(doctors.size());
+    }
+    
+    public void loadPatient(){
+         setPatients(patientFacade.loadPatient());
+       
+    }
+    
+   
+
+    /**
+     * @return the doctors
+     */
+    public List<Doctor> getDoctors() {
+        return doctors;
+    }
+
+    /**
+     * @param doctors the doctors to set
+     */
+    public void setDoctors(List<Doctor> doctors) {
+        this.doctors = doctors;
+    }
+
+    /**
+     * @return the medicalRecords
+     */
+    public List<MedicalRecord> getMedicalRecords() {
+        return medicalRecords;
+    }
+
+    /**
+     * @param medicalRecords the medicalRecords to set
+     */
+    public void setMedicalRecords(List<MedicalRecord> medicalRecords) {
+        this.medicalRecords = medicalRecords;
+    }
+
+   
+
+    /**
+     * @return the patientFacade
+     */
+    public PatientFacade getPatientFacade() {
+        return patientFacade;
+    }
+
+    /**
+     * @param patientFacade the patientFacade to set
+     */
+    public void setPatientFacade(PatientFacade patientFacade) {
+        this.patientFacade = patientFacade;
+    }
+
+    /**
+     * @return the patients
+     */
+    public ArrayList<Patient> getPatients() {
+        return patients;
+    }
+
+    /**
+     * @param patients the patients to set
+     */
+    public void setPatients(ArrayList<Patient> patients) {
+        this.patients = patients;
+    }
+
+    /**
+     * @return the medicalRecord
+     */
+   
+}
